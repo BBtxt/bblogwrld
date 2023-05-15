30736559726475cb9b6fe79cc3b1e7507847a86b
Brando <bbaker.sh@gmail.com>
2023-05-15 15:36:00 -0700

    im scared

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/display-name */
 import React from 'react'
 import { useMDXComponent } from 'next-contentlayer/hooks'
-import { ComponentMap } from 'mdx-bundler/client'
 import { coreContent } from '@/lib/utils/contentlayer'
 import Image from './Image'
 import CustomLink from './Link'
